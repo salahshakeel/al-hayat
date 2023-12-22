93215f442f69aef6d38f5286a673c23c253ceb38
salahshakeel <99357263+salahshakeel@users.noreply.github.com>
2023-12-22 22:03:21 +0500

    Updated navbar2

--- src/components/SubNavbar2.jsx
+++ src/components/SubNavbar2.jsx
@@ -6,7 +6,7 @@ const SubNavbar2 = () => {
   const pathnames = location.pathname.split('/').filter((x) => x);
 
   return (
-    <div className="flex justify-center items-center ">
+    <div className="flex justify-center items-center">
       <nav className="flex" aria-label="Breadcrumb">
         <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
@@ -21,10 +21,9 @@ const SubNavbar2 = () => {
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
-                 <svg className="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-                </svg>
-
+                <path
+                  d="M19.707 9.293l-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+                />
               </svg>
               Home
             </Link>
@@ -43,12 +42,18 @@ const SubNavbar2 = () => {
                     fill="none"
                     viewBox="0 0 6 10"
                   >
-                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-        </svg>
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 9l4-4-4-4"
+                    />
                   </svg>
                   {index === pathnames.length - 1 ? (
-                    <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">{name}</span>
+                    <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                      {name}
+                    </span>
                   ) : (
                     <Link
                       to={routeTo}
